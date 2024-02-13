0a4f9fb35dd6fda499dd99d079f82bbd533bb155
Federico Deniard <fededeniard@gmail.com>
2024-02-13 01:26:57 -0300

    WIP -Sequences are now preloaded -Interface changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,52 @@ import { i_iv_vii_iii_vi_ii_v_i_sequence } from "./components/chords";
 import { useEffect } from "react";
 
 function App() {
-  ii_v_i_sequence();
-
+  // First Sequence
   const [chord, setChord] = useState("");
 
   const firstSequence = () => {
-    const getChord = ii_v_i_sequence();
+    const getChord = ii_v_i_sequence("");
     setChord(getChord);
   };
 
-  const [chord2, setChord2] = useState();
+  // Second Sequence
+  const [chord2, setChord2] = useState("");
 
   const secondSequence = () => {
     const getChord2 = i_iv_vii_iii_vi_ii_v_i_sequence();
     setChord2(getChord2);
   };
 
+  // Preload chords
+
+  useEffect(() => {
+    if (chord === "") {
+      firstSequence();
+    }
+    if (chord2 === "") {
+      secondSequence();
+    }
+  });
+
+  //
+
   return (
     <>
-      <h1>ChordMaster</h1>
-      <p>II V I</p>
-      <p>{chord}</p>
-      <button onClick={firstSequence}>Get Chord</button>
-      <p> I IV VII III VI II V I</p>
-      <p>{chord2}</p>
-      <button onClick={secondSequence}>Get Secondary Chord</button>
+      <div className="app">
+        <h1 className="chordmaster">ChordMaster</h1>
+        <div className="container">
+          {" "}
+          <p>II V I</p>
+          <p>{chord}</p>
+          <button onClick={firstSequence}>Get Chord</button>
+        </div>
+        <div className="container">
+          <h4>Circle of fifths</h4>
+          <p> i iv VII III VI ii V i</p>
+          <p>{chord2}</p>
+          <button onClick={secondSequence}>Get Chord</button>
+        </div>
+      </div>
     </>
   );
 }
